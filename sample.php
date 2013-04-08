c0d9be358d541579b5bb60ee3c1a7eee5792c060
pallavik <pallavik@glam.com>
2013-04-08 15:59:52 +0530

    added codesniffer fixes

--- sample.php
+++ sample.php
@@ -1,10 +1,24 @@
 <?php
+/**
+* MyClass Class Doc Comment
+*
+* PHP version 5
+*
+* @category Class
+* @package  MyPackage
+* @author   pallavi kolekar Other <pallavik@glam.com>
+* @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+* @link     http://www.hashbangcode.com/
+* 
+*/
+#this is comment
 echo "This is function call example";
-$res = add( 3,5 );
+$res = add(3, 5);
 echo "The addition is: ".$res;
-function add( $a,$b){
- $c = $a + $b;
- return $c;
+function add($a, $b)
+{
+            $c = $a + $b;
+            return $c;
 }
 echo "this is another changes in file";
 ?>
